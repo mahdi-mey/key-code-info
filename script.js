75ba5b5344d7336682bf6aae29972fa851685739
Mahdi <mahdimey17@gmail.com>
2023-10-02 16:31:51 +0330

    selected dom elements in js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const $ = document
+const locationKey = $.querySelector('#location')
+const userTitle = $.querySelector('title')
+
+const keyCodeElem = $.querySelector('#keyCode') 
+const keyElem = $.querySelector('#key') 
+const locationElem = $.querySelector('#location') 
+const whichElem = $.querySelector('#which') 
+const codeElem = $.querySelector('#code') 
 const starter = document.querySelector('#starter')
 
 document.body.addEventListener('keydown', function (event) {
