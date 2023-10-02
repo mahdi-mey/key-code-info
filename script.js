8e8177cc7d6e939689fcd4d34d173beacd8eff96
Mahdi <mahdimey17@gmail.com>
2023-10-02 16:34:32 +0330

    completed function

--- script.js
+++ script.js
@@ -12,4 +12,21 @@ const starter = document.querySelector('#starter')
 document.body.addEventListener('keydown', function (event) {
 	console.log(event);
     starter.style.display = 'none'
+
+    heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+
+    let eventKeyCode = event.keyCode
+	let eventKey = event.key
+	let eventLocation = event.location
+	let eventWhich = event.which
+	let eventCode = event.code
+
+	keyCodeElem.textContent = eventKeyCode
+	keyElem.textContent = eventKey
+	locationElem.textContent = eventLocation
+	whichElem.textContent = eventWhich
+	codeElem.textContent = eventCode
+
 })
